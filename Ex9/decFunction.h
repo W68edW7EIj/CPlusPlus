f9a029d1c75128ca7574421bbdf0c4ad35d0c27c
WJH <3506456886@qq.com>
2025-11-11 11:27:10 +0800

    feat: VSCode中对C++的编译配置，支持对子文件夹递归编译

--- Ex9/decFunction.h
+++ Ex9/decFunction.h
@@ -9,7 +9,7 @@ void func(int a2, std::string b2);
 void func(int a3, std::string b3, float c3);
 // int func(int a);
 // float func(int a);
-// 报错: 无法重载仅按返回类型区分的函数
+// 报错: 无法重载仅按返回类型区分的函数，因为返回类型不参与重载
 int func(double a);
 
 #endif
--- Ex9/main.cpp
+++ Ex9/main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     func(1);
-    func(1, '2');
-    func(1, '2', 3.0);
-    return 1;
+    func(1, "2");
+    func(1, "2", 3.0);
+    return 0;
 }
